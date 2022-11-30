f3a8333e5972e9e576fe7425cc5849218ef2bb21
jerryzhuo <jerryzhuo@tencent.com>
2022-12-01 01:13:46 +0800

    fix: error

--- client/client.go
+++ client/client.go
@@ -4,157 +4,70 @@ import (
 	"flag"
 	"fmt"
 	"net"
+	"strconv"
 	"strings"
-	"udp-dig/common"
-)
-
-const hintInfo = `========== input command number ==========
-===        [0]: link server            ===
-===        [1]: show all client        ===
-===        [2]: connect client         ===
-==========================================`
-
-var (
-	clientAddr *net.UDPAddr
-	serverAddr *net.UDPAddr
-
-	serverConn *net.UDPConn
 )
 
 func main() {
 	var port int
 	var serverIp string
 	var serverPort int
-	flag.IntVar(&port, "port", -1, "")
-	flag.StringVar(&serverIp, "serverIp", "127.0,0.1", "")
-	flag.IntVar(&serverPort, "serverPort", 3356, "")
+	flag.IntVar(&port, "port", 9981, "")
+	flag.StringVar(&serverIp, "serverIp", "127.0.0.1", "")
+	flag.IntVar(&serverPort, "serverPort", 9980, "")
 	flag.Parse()
 
-	clientAddr = &net.UDPAddr{IP: net.IPv4zero, Port: port}
-	serverAddr = &net.UDPAddr{IP: net.ParseIP(serverIp), Port: serverPort}
+	localAddr := &net.UDPAddr{IP: net.IPv4zero, Port: port}
 
-	fmt.Println(hintInfo)
-	for {
-		var command int
-		fmt.Scan(&command)
+	serverConn, err := net.DialUDP("udp", localAddr, &net.UDPAddr{IP: net.ParseIP(serverIp), Port: serverPort})
+	if err != nil {
+		panic(err)
+	}
+	serverConn.Write(nil)
 
-		if command < 0 || command > 2 {
-			continue
+	data := make([]byte, 1024)
+	dataLen, _, err := serverConn.ReadFromUDP(data)
+	if err != nil {
+		panic(err)
+	}
+	serverConn.Close()
+
+	remoteAddr := parseAddr(string(data[:dataLen]))
+	conn, err := net.DialUDP("udp", localAddr, remoteAddr)
+	if err != nil {
+		panic(err)
+	}
+
+	conn.Write(nil)
+
+	go func() {
+		for {
+			data := make([]byte, 1024)
+			dataLen, _, _ := conn.ReadFromUDP(data)
+			if dataLen != 0 {
+				fmt.Printf("%s->%s\n", remoteAddr, data[:dataLen])
+			}
 		}
+	}()
 
-		if command == 0 {
-			conn, err := net.DialUDP("udp", clientAddr, serverAddr)
-			if err != nil {
-				panic(err)
-			}
+	for {
+		var msg string
+		fmt.Scanln(&msg)
 
-			if _, err = conn.Write([]byte(common.ConvertMsgInfo(
-				&common.MsgInfo{Type: common.ConnectReq}))); err != nil {
-				panic(err)
-			}
+		conn.Write([]byte(msg))
 
-			serverConn = conn
-
-			go serverMsgDealWith()
-		} else if command == 1 {
-			if _, err := serverConn.Write([]byte(common.ConvertMsgInfo(
-				&common.MsgInfo{Type: common.GetClientListReq}))); err != nil {
-				panic(err)
-			}
-		} else if command == 2 {
-			var dstAddrStr string
-			fmt.Scan(&dstAddrStr)
-
-			if _, err := serverConn.Write([]byte(common.ConvertMsgInfo(
-				&common.MsgInfo{Type: common.ConnectClientReq, Data: dstAddrStr}))); err != nil {
-				panic(err)
-			}
-			serverConn.Close()
-
-			conn, err := net.DialUDP("udp", clientAddr, common.ParseAddr(dstAddrStr))
-			if err != nil {
-				panic(err)
-			}
-			if _, err = conn.Write([]byte(common.ConvertMsgInfo(
-				&common.MsgInfo{Type: common.IgnoreInfo}))); err != nil {
-				panic(err)
-			}
-
-			go msgDealWith(conn)
-
-			for {
-				var data string
-				fmt.Scan(&data)
-
-				if data == "exit" {
-					conn.Close()
-					break
-				}
-
-				if _, err = conn.Write([]byte(common.ConvertMsgInfo(
-					&common.MsgInfo{Type: common.DataInfo, Data: data}))); err != nil {
-					panic(err)
-				}
-			}
+		if msg == "exit" {
+			conn.Close()
+			return
 		}
 	}
 }
 
-func msgDealWith(conn *net.UDPConn) {
-	for {
-		msgInfo, err := common.GetMsgInfoFromConn(conn)
-		if err != nil {
-			continue
-		}
-
-		if msgInfo.Type == common.CloseConnection {
-			return
-		}
-
-		if msgInfo.Type == common.DataInfo {
-			fmt.Printf("[%s]->%s\n", msgInfo.RemoteAddr.String(), msgInfo.Data)
-		}
-	}
-}
-
-func serverMsgDealWith() {
-	for {
-		msgInfo, err := common.GetMsgInfoFromConn(serverConn)
-		if err != nil {
-			return
-		}
-
-		if msgInfo.Type == common.GetClientListRsp {
-			fmt.Printf("->%s\n", strings.Join(strings.Split(msgInfo.Data, "|"), " "))
-		} else if msgInfo.Type == common.ConnectClientReq {
-			serverConn.Close()
-
-			conn, err := net.DialUDP("udp", clientAddr, common.ParseAddr(msgInfo.Data))
-			if err != nil {
-				panic(err)
-			}
-
-			if _, err = conn.Write([]byte(common.ConvertMsgInfo(
-				&common.MsgInfo{Type: common.IgnoreInfo}))); err != nil {
-				panic(err)
-			}
-
-			go msgDealWith(conn)
-
-			for {
-				var data string
-				fmt.Scan(&data)
-
-				if data == "exit" {
-					conn.Close()
-					return
-				}
-
-				if _, err = conn.Write([]byte(common.ConvertMsgInfo(
-					&common.MsgInfo{Type: common.DataInfo, Data: data}))); err != nil {
-					panic(err)
-				}
-			}
-		}
+func parseAddr(addr string) *net.UDPAddr {
+	t := strings.Split(addr, ":")
+	port, _ := strconv.Atoi(t[1])
+	return &net.UDPAddr{
+		IP:   net.ParseIP(strings.Split(addr, ":")[0]),
+		Port: port,
 	}
 }
--- server/server.go
+++ server/server.go
@@ -4,17 +4,11 @@ import (
 	"flag"
 	"fmt"
 	"net"
-	"strings"
-	"udp-dig/common"
-)
-
-var (
-	port      int
-	clientMap = make(map[string]*net.UDPAddr)
 )
 
 func main() {
-	flag.IntVar(&port, "port", 3356, "")
+	var port int
+	flag.IntVar(&port, "port", 9980, "")
 	flag.Parse()
 
 	listener, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.IPv4zero, Port: port})
@@ -22,40 +16,21 @@ func main() {
 		panic(err)
 	}
 
+tag:
 	for {
-		msgInfo, err := common.GetMsgInfoFromConn(listener)
-		if err != nil {
-			listener.WriteToUDP([]byte(common.ConvertMsgInfo(
-				&common.MsgInfo{Type: common.ErrorInfo, Data: err.Error()})), msgInfo.RemoteAddr)
-			continue
+		peers := make([]*net.UDPAddr, 2)
+		data := make([]byte, 1024)
+		for i := 0; i < 2; i++ {
+			_, remoteAddr, err := listener.ReadFromUDP(data)
+			if err != nil {
+				fmt.Println(err)
+				continue tag
+			}
+			peers[i] = remoteAddr
 		}
 
-		if msgInfo.Type == common.ConnectReq {
-			clientMap[msgInfo.RemoteAddr.String()] = msgInfo.RemoteAddr
-
-			listener.WriteToUDP([]byte(common.ConvertMsgInfo(
-				&common.MsgInfo{Type: common.ConnectRsp, Data: "ok"})), msgInfo.RemoteAddr)
-		} else if msgInfo.Type == common.GetClientListReq {
-			var clientList []string
-			for k := range clientMap {
-				clientList = append(clientList, k)
-			}
-
-			listener.WriteToUDP([]byte(common.ConvertMsgInfo(&common.MsgInfo{Type: common.GetClientListRsp,
-				Data: strings.Join(clientList, "|")})), msgInfo.RemoteAddr)
-		} else if msgInfo.Type == common.ConnectClientReq {
-			dstRemoteAddr, ok := clientMap[msgInfo.Data]
-			if !ok {
-				listener.WriteToUDP([]byte(common.ConvertMsgInfo(&common.MsgInfo{Type: common.ErrorInfo,
-					Data: fmt.Sprintf("addr no exist: %s", msgInfo.Data)})), msgInfo.RemoteAddr)
-				continue
-			}
-
-			listener.WriteToUDP([]byte(common.ConvertMsgInfo(
-				&common.MsgInfo{Type: common.ConnectClientReq, Data: msgInfo.RemoteAddr.String()})), dstRemoteAddr)
-
-			listener.WriteToUDP([]byte(common.ConvertMsgInfo(
-				&common.MsgInfo{Type: common.ConnectClientRsp, Data: "ok"})), msgInfo.RemoteAddr)
-		}
+		fmt.Printf("[%s]<--->[%s]\n", peers[0].String(), peers[1].String())
+		listener.WriteToUDP([]byte(peers[1].String()), peers[0])
+		listener.WriteToUDP([]byte(peers[0].String()), peers[1])
 	}
 }
